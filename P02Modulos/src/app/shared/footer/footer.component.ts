36185e3d36b8b54cc97ba085f2339a56f173b350
Jose Luis Villapalos Llobregat <jlvillapalos@indra.es>
2017-11-28 12:29:01 +0100

    Actualización de los cambios

---
+++ P02Modulos/src/app/shared/footer/footer.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  public autor:string;
+  public empresa:string;
+  constructor() { }
+
+  ngOnInit() {
+    this.empresa = 'Indra';
+    this.autor='jlvillapalos';
+  }
+
+}
